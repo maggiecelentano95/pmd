e348506d39d135e598f0921f1ec9567994083b7e
= <maggiejane1595@gmail.com>
2026-02-01 14:55:08 -0500

    Added ability to add cusotm portraits.  This includes extending the rom size to 64 mb.

---
+++ data/custom_portraits.s
@@ -0,0 +1,128 @@
+.section .custom_portraits, "a"
+.align 4
+
+.global sTorchicExcitedPal_Custom
+sTorchicExcitedPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sTorchicExcitedGfx_Custom
+sTorchicExcitedGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiNormalPal_Custom
+sEnteiNormalPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiNormalGfx_Custom
+sEnteiNormalGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiHappyPal_Custom
+sEnteiHappyPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiHappyGfx_Custom
+sEnteiHappyGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiAngryPal_Custom
+sEnteiAngryPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiPainedGfx_Custom
+sEnteiPainedGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiPainedPal_Custom
+sEnteiPainedPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiAngryGfx_Custom
+sEnteiAngryGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiWorriedPal_Custom
+sEnteiWorriedPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiWorriedGfx_Custom
+sEnteiWorriedGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiSadPal_Custom
+sEnteiSadPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiSadGfx_Custom
+sEnteiSadGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiCryingPal_Custom
+sEnteiCryingPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiCryingGfx_Custom
+sEnteiCryingGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiShoutingPal_Custom
+sEnteiShoutingPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiShoutingGfx_Custom
+sEnteiShoutingGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiTearyEyePal_Custom
+sEnteiTearyEyePal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiTearyEyeGfx_Custom
+sEnteiTearyEyeGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiDeterminedPal_Custom
+sEnteiDeterminedPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiDeterminedGfx_Custom
+sEnteiDeterminedGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiJoyousPal_Custom
+sEnteiJoyousPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiJoyousGfx_Custom
+sEnteiJoyousGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiInspiredPal_Custom
+sEnteiInspiredPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiInspiredGfx_Custom
+sEnteiInspiredGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
+
+.global sEnteiSurprisedPal_Custom
+sEnteiSurprisedPal_Custom:
+.incbin "data/kao/customportraits/grookey_normal.pal"
+
+.align 4
+.global sEnteiSurprisedGfx_Custom
+sEnteiSurprisedGfx_Custom:
+.incbin "data/kao/customportraits/grookey_normal.at4px"
